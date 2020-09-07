395b6b4a148f6af9c1749fdba863f4c8af1ec007
dmarina7kg <David.Marina@esa.int>
2020-09-07 09:28:12 +0200

    correction

--- releng/scripts/init-environment.sh
+++ releng/scripts/init-environment.sh
@@ -33,7 +33,7 @@ echo "RAP Runtime p2 repository: ${RAP_REPOSITORY}"
 export REPOSITORY_BASE_PATH=${REPOSITORY_BASE_PATH:-"/shared/rt/rap/incubator/nightly"}
 echo "p2 repository base path: ${REPOSITORY_BASE_PATH}"
 
-export INCUBATOR_REPOSITORY_BASE_PATH=${REPOSITORY_BASE_PATH:-"/home/data/httpd/download.eclipse.org/rt/rap/incubator/nightly"}
+export INCUBATOR_REPOSITORY_BASE_PATH=${INCUBATOR_REPOSITORY_BASE_PATH:-"/home/data/httpd/download.eclipse.org/rt/rap/incubator/nightly"}
 echo "Incubator p2 repository base path: ${INCUBATOR_REPOSITORY_BASE_PATH}"
 
 export NUM_TO_KEEP=${NUM_TO_KEEP:-"5"}
